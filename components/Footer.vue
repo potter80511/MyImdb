12a809df4ade9340250b657f24cf9357405838fc
簡豪江 <potter80511@gmail.com>
2020-09-07 23:16:46 +0800

    移除FB, youtube, IG

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,24 +2,9 @@
   <footer>
     <div class="container">
       <div class="social">
-        <div class="facebook">
-          <a href="https://www.facebook.com/haochiang.chien" target="_blank">
-            <font-awesome-icon :icon="['fab', 'facebook-square']" />
-          </a>
-        </div>
         <div class="github">
           <a href="https://github.com/potter80511" target="_blank">
             <font-awesome-icon :icon="['fab', 'github']" />
-          </a>
-        </div>
-        <div class="youtube">
-          <a href="https://www.youtube.com/channel/UCzQj08-n95D_aftgctWYdng" target="_blank">
-            <font-awesome-icon :icon="['fab', 'youtube']" />
-          </a>
-        </div>
-        <div class="instagram">
-          <a href="https://www.instagram.com/johnny19910511/" target="_blank">
-            <font-awesome-icon :icon="['fab', 'instagram']" />
           </a>
         </div>
         <div class="line">
